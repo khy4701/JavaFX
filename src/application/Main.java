44721a24e93c975b6a96e5050a7ad41ef8e5f42e
hoyoung <khy4701@nate.com>
2016-06-21 14:51:26 +0900

    Java InputControl

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			
+						
 			Parent root = FXMLLoader.load(getClass().getResource("root.fxml"));
 			
 //			BorderPane root = new BorderPane();
